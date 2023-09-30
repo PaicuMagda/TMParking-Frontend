d437896d51dc3818c4303bc349e98379a5ec3922
PaicuMagda <paicumagda962@gmail.com>
2023-09-30 18:09:24 +0300

    deleted create-account

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,9 @@ import { LoginComponent } from './Components/login-register-components/login/log
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
-import { CreateAccountComponent } from './Components/login-register-components/create-account/create-account/create-account.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, CreateAccountComponent],
+  declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
