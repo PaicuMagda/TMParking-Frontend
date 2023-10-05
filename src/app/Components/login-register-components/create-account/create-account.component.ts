077ecb75ea73b7da8acc6f70e767d016698b84bc
PaicuMagda <paicumagda962@gmail.com>
2023-10-05 23:10:12 +0300

    created formBuilder

--- src/app/Components/login-register-components/create-account/create-account.component.ts
+++ src/app/Components/login-register-components/create-account/create-account.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
-  styleUrls: ['./create-account.component.scss']
+  styleUrls: ['./create-account.component.scss'],
 })
 export class CreateAccountComponent {
+  createAccountForm: FormGroup;
 
+  constructor(private formBuider: FormBuilder) {
+    this.createAccountForm = this.formBuider.group({
+      email: ['', [Validators.required, Validators.email]],
+      username: [
+        '',
+        [Validators.required, Validators.pattern(/^[a-zA-Z]{4,30}$/)],
+      ],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
 }
