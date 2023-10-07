cd1366a85db3c735de114c16249586e7137da8ad
Magda Paicu <paicumagda962@gmail.com>
2023-10-07 15:02:02 +0300

    create nav-bar for no user login

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [ReactiveFormsModule, MatInputModule, MatFormFieldModule],
+  exports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+  ],
 })
 export class MaterialModule {}
