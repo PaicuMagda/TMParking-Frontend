40b2186a4ee248fcb03d300ddeae22cedac167f0
Magda Paicu <paicumagda962@gmail.com>
2023-10-11 17:27:12 +0300

    create dialog for add new parking space

---
+++ src/app/shared/components/add-new-parking-space-dialog/add-new-parking-space-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-parking-space-dialog',
+  templateUrl: './add-new-parking-space-dialog.component.html',
+  styleUrls: ['./add-new-parking-space-dialog.component.scss']
+})
+export class AddNewParkingSpaceDialogComponent {
+
+}
