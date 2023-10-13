946603dcdc118562318ef445e937ea384a014e5b
Magda Paicu <paicumagda962@gmail.com>
2023-10-13 12:22:42 +0300

    create dialog for add new parking space

--- src/app/shared/components/add-new-parking-space-dialog/add-new-parking-space-dialog.component.ts
+++ src/app/shared/components/add-new-parking-space-dialog/add-new-parking-space-dialog.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-new-parking-space-dialog',
   templateUrl: './add-new-parking-space-dialog.component.html',
-  styleUrls: ['./add-new-parking-space-dialog.component.scss']
+  styleUrls: ['./add-new-parking-space-dialog.component.scss'],
 })
-export class AddNewParkingSpaceDialogComponent {
+export class AddNewParkingSpaceDialogComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit() {}
 }
