50fa62a8dec46b6ec81578d12c9ffd62d0be348b
Magda Paicu <paicumagda962@gmail.com>
2024-05-09 20:47:14 +0300

    feat(charts): create multiple types of charts for each component

--- src/app/Components/charts/charts.component.ts
+++ src/app/Components/charts/charts.component.ts
@@ -1,43 +1,8 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
   styleUrls: ['./charts.component.scss'],
 })
-export class ChartsComponent {
-  @ViewChild('pieChart') pieChart: ElementRef;
-
-  drawChart = () => {
-    const data = google.visualization.arrayToDataTable([
-      ['Task', 'Hours per Day'],
-      ['Work', 11],
-      ['Eat', 2],
-      ['Commute', 2],
-      ['Watch TV', 2],
-      ['Sleep', 7],
-    ]);
-
-    const options = {
-      colors: [
-        '#e6194B',
-        '#3cb44b',
-        '#ffe119',
-        '#4363d8',
-        '#f58231',
-        '#911eb4',
-      ],
-    };
-
-    const chart = new google.visualization.PieChart(
-      this.pieChart.nativeElement
-    );
-
-    chart.draw(data, options);
-  };
-
-  ngAfterViewInit() {
-    google.charts.load('current', { packages: ['corechart'] });
-    google.charts.setOnLoadCallback(this.drawChart);
-  }
-}
+export class ChartsComponent {}
--- src/app/Components/charts/reservations-charts/reservations-charts.component.ts
+++ src/app/Components/charts/reservations-charts/reservations-charts.component.ts
@@ -6,7 +6,168 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./reservations-charts.component.scss'],
 })
 export class ReservationsChartsComponent {
-  @ViewChild('chart') pieChart: ElementRef;
+  @ViewChild('chart') chart: ElementRef;
+  selectedChartType: string = '';
+
+  drawBarChart() {
+    const data = google.visualization.arrayToDataTable([
+      ['Task', 'Hours per Day'],
+      ['Work', 11],
+      ['Eat', 2],
+      ['Commute', 2],
+      ['Watch TV', 2],
+      ['Sleep', 7],
+    ]);
+
+    const options = {
+      colors: [
+        '#e6194B',
+        '#3cb44b',
+        '#ffe119',
+        '#4363d8',
+        '#f58231',
+        '#911eb4',
+      ],
+    };
+
+    const chart = new google.visualization.BarChart(this.chart.nativeElement);
+
+    chart.draw(data, options);
+  }
+
+  drawLineChart() {
+    const data = google.visualization.arrayToDataTable([
+      ['Task', 'Hours per Day'],
+      ['Work', 11],
+      ['Eat', 2],
+      ['Commute', 2],
+      ['Watch TV', 2],
+      ['Sleep', 7],
+    ]);
+
+    const options = {
+      colors: [
+        '#e6194B',
+        '#3cb44b',
+        '#ffe119',
+        '#4363d8',
+        '#f58231',
+        '#911eb4',
+      ],
+    };
+
+    const chart = new google.visualization.LineChart(this.chart.nativeElement);
+
+    chart.draw(data, options);
+  }
+
+  drawColumnChart() {
+    const data = google.visualization.arrayToDataTable([
+      ['Task', 'Hours per Day'],
+      ['Work', 11],
+      ['Eat', 2],
+      ['Commute', 2],
+      ['Watch TV', 2],
+      ['Sleep', 7],
+    ]);
+
+    const options = {
+      colors: [
+        '#e6194B',
+        '#3cb44b',
+        '#ffe119',
+        '#4363d8',
+        '#f58231',
+        '#911eb4',
+      ],
+    };
+
+    const chart = new google.visualization.ColumnChart(
+      this.chart.nativeElement
+    );
+
+    chart.draw(data, options);
+  }
+
+  drawAreaChart() {
+    const data = google.visualization.arrayToDataTable([
+      ['Task', 'Hours per Day'],
+      ['Work', 11],
+      ['Eat', 2],
+      ['Commute', 2],
+      ['Watch TV', 2],
+      ['Sleep', 7],
+    ]);
+
+    const options = {
+      colors: [
+        '#e6194B',
+        '#3cb44b',
+        '#ffe119',
+        '#4363d8',
+        '#f58231',
+        '#911eb4',
+      ],
+    };
+
+    const chart = new google.visualization.AreaChart(this.chart.nativeElement);
+
+    chart.draw(data, options);
+  }
+
+  drawScatterChart() {
+    const data = google.visualization.arrayToDataTable([
+      ['Task', 'Hours per Day'],
+      ['Work', 11],
+      ['Eat', 2],
+      ['Commute', 2],
+      ['Watch TV', 2],
+      ['Sleep', 7],
+    ]);
+
+    const options = {
+      colors: [
+        '#e6194B',
+        '#3cb44b',
+        '#ffe119',
+        '#4363d8',
+        '#f58231',
+        '#911eb4',
+      ],
+    };
+
+    const chart = new google.visualization.ScatterChart(
+      this.chart.nativeElement
+    );
+
+    chart.draw(data, options);
+  }
+
+  drawHistogram() {
+    const data = google.visualization.arrayToDataTable([
+      ['Task', 'Hours per Day'],
+      ['Work', 11],
+      ['Eat', 2],
+      ['Commute', 2],
+      ['Watch TV', 2],
+      ['Sleep', 7],
+    ]);
+
+    const options = {
+      colors: [
+        '#e6194B',
+        '#3cb44b',
+        '#ffe119',
+        '#4363d8',
+        '#f58231',
+        '#911eb4',
+      ],
+    };
+
+    const chart = new google.visualization.Histogram(this.chart.nativeElement);
+
+    chart.draw(data, options);
+  }
 
   drawChart = () => {
     const data = google.visualization.arrayToDataTable([
@@ -29,12 +190,36 @@ export class ReservationsChartsComponent {
       ],
     };
 
-    const chart = new google.visualization.ComboChart(
-      this.pieChart.nativeElement
-    );
+    const chart = new google.visualization.PieChart(this.chart.nativeElement);
 
     chart.draw(data, options);
   };
+
+  changeChartType(chartType: string) {
+    this.selectedChartType = chartType;
+    switch (chartType) {
+      case 'bar':
+        this.drawBarChart();
+        break;
+      case 'line':
+        this.drawLineChart();
+        break;
+      case 'column':
+        this.drawColumnChart();
+        break;
+      case 'area':
+        this.drawAreaChart();
+        break;
+      case 'scatter':
+        this.drawScatterChart();
+        break;
+      case 'histogram':
+        this.drawHistogram();
+        break;
+      default:
+        break;
+    }
+  }
 
   ngAfterViewInit() {
     google.charts.load('current', { packages: ['corechart'] });
